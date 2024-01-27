1bf85faf9dc464b54de5f911655eafcbb3bfe78a
dwu35 <dwu35@buffalo.edu>
2024-01-27 15:32:29 -0500

    add tile, player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontal;
+    private float speed = 8f;
+    private bool isFacingRight = true;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        
+        Flip();
+    }
+
+    private void FixedUpdate() {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    private void Flip() {
+        if(isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x = -1f;
+            transform.localScale = localScale;
+        }
+    }
+}
