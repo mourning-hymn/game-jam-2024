31db161ef2ded46eeeefd2d40c44359f85086d51
ethanlab <ethanlab@buffalo.edu>
2024-01-28 14:37:38 -0500

    player can take damage

--- Assets/Scripts/WeaponHit.cs
+++ Assets/Scripts/WeaponHit.cs
@@ -7,9 +7,13 @@ public class WeaponHit : MonoBehaviour
     public PlayerHealth playerHealth;
     private void OnCollisionEnter2D(Collision2D collision)
     {
+
         if (collision.gameObject.tag == "Player")
         {
+            Debug.Log("got hit");
             playerHealth.TakeDamage(1);
         }
     }
+
+
 }
